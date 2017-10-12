8dea8aed2f7dc5aa209efd2ece9e8c1ed9856868
Will Schmierer <will@webdevstudios.com>
2017-10-12 13:31:47 -0400

    adds fanciness to the fancy nav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation();
+$( document ).foundation();
+
+var $navList = $( '.menu-fancy' );
+
+$navList.on( 'click', 'li:not(.is-selected)', function( e ) {
+	$navList.find( '.is-selected' ).removeClass( 'is-selected' );
+	$( e.currentTarget ).addClass( 'is-selected' );
+} );
+
